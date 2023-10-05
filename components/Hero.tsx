4fd37f593320dec296e3a78ea8827409484574d5
Anton Rubchenko <rubchenko.anton@gmail.com>
2023-10-05 14:53:08 +0300

    chore: add Header and Footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,11 +28,12 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className='hero__image-container'></div>
-      <div className='hero__image'>
-        <Image src='/hero.png' alt='hero'
-        fill className='object-contain'/>
-        <div className='hero__image-overlay'/>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/hero.png' alt='hero'
+            fill className='object-contain' />
+        </div>
+        <div className='hero__image-overlay' />
       </div>
     </div>
   )
